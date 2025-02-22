2973c66fe701ee271adf2fae02c01862c9af4063
boris2442 <aubinborissimotsebo@gmail.com>
2025-02-22 01:21:01 +0100

    suite de la formation

--- upload.php
+++ upload.php
@@ -4,8 +4,31 @@
 // echo "</pre>";
 // die;
 //verifier  si un fichier a ete envoyer
-if($_FILES["images"])&& $_FILES["images"]["0"]===){
-    
+if (isset($_FILES["images"]) && $_FILES["images"]["error"] === 0) {
+    var_dump($_FILES);
+    //proceder aux verifications
+    //on verifie toujous l'extention et le type mine
+    $allowed=[
+    "jpg"=>"image/jpeg",
+    "jpeg"=>"image/jpeg",
+    "png"=>"image/png",
+    "pdf"=>"image/pdf"
+   ];
+   $filename=$_FILES["images"]["name"];
+   $filetype=$_FILES["images"]["type"];
+   $filesize=$_FILES["images"]["size"];
+   //on verifie si la taille du fichier est inferieure a 1Mo
+   $extension=pathinfo($filename, PATHINFO_EXTENSION);
+   //on verifie si l'extention est autorise
+   if(!array_key_exists($extension, $allowed)||(!in_array($filetype, $allowed))){
+    //soit l'extension  soit le type est incorrect
+       die("le type de fichier n'est pas autorise");
+   }
+   //le type est correct!
+   //on pourrait verifier la taill du fichier
+   if($filesize >2*1024*1024){
+    die("la taille du fichier ne doit pas exceder 2mo ");
+   }
 }
 
 
@@ -17,31 +40,34 @@ if($_FILES["images"])&& $_FILES["images"]["0"]===){
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <h1>ajout de fichier</h1>
-<form action="" method="post" enctype="multipart/form-data">
-<div class="">
-    <label for="fichier">Fichier</label>
-    <input type="file" name="images" id="fichier" multiple accept="">
-    <button type="submit">Envoyer</button>
-</div>
-<div class="">
-    <label for="fichier">Fichier</label>
-    <input type="file" name="files" id="fichier">
-    <button type="submit">Envoyer</button>
-</div>
+    <form action="" method="post" enctype="multipart/form-data">
+        <div class="">
+            <label for="fichier">Fichier</label>
+            <input type="file" name="images" id="fichier" multiple accept="">
+            <button type="submit">Envoyer</button>
+        </div>
+        <div class="">
+            <label for="fichier">Fichier</label>
+            <input type="file" name="files" id="fichier">
+            <button type="submit">Envoyer</button>
+        </div>
 
 
 
 
 
 
-</form   
-</body>
+    </form
+        </body>
+
 </html>
 >
